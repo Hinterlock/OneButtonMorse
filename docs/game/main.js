44325fbd77f57ee6e307eaf75947a4f002a63280
Hinterlock <102774385+Hinterlock@users.noreply.github.com>
2023-11-13 18:55:22 -0800

    made main morse

--- docs/game/main.js
+++ docs/game/main.js
@@ -1,8 +1,9 @@
-title = "Number Cruncher";
+title = "Morse Encoder!";
 
 description = `
-[Tap]  Zero
-[Hold] One
+[Tap]  Dot
+[Hold] Dash
+[Wait] Pause
 `;
 
 characters = []; // for sprites (none atm)
@@ -23,6 +24,47 @@ const questionTime = 600;
 const maxTime = 2000;
 const holdTime = 10;
 
+// Morse Code dictionary:
+const morseToABC = {
+	'.-':		'a',
+	'-...':		'b',
+	'-.-.':		'c',
+	'-..':		'd',
+	'.':		'e',
+	'..-.':		'f',
+	'--.':		'g',
+	'....':		'h',
+	'..':		'i',
+	'.---':		'j',
+	'-.-':		'k',
+	'.-..':		'l',
+	'--':		'm',
+	'-.':		'n',
+	'---':		'o',
+	'.--.':		'p',
+	'--.-':		'q',
+	'.-.':		'r',
+	'...':		's',
+	'-':		't',
+	'..-':		'u',
+	'...-':		'v',
+	'.--':		'w',
+	'-..-':		'x',
+	'-.--':		'y',
+	'--..':		'z',
+/*	'.----':	'1',
+	'..---':	'2',
+	'...--':	'3',
+	'....-':	'4',
+	'.....':	'5',
+	'-....':	'6',
+	'--...':	'7',
+	'---..':	'8',
+	'----.':	'9',
+	'-----':	'0',*/
+};
+let ABCToMorse;
+
 // variables
 let isPressing;		// checking if button is held
 let held; 			// counts how long button is held, to differentiate dots and dashes
@@ -32,25 +74,31 @@ let answer; 		// player input
 let correctAnswer;	// what input should match
 let binaryTxt;		// display text in binary
 let b10Txt;			// display text in base 10
+let paused;			// has a pause been registered yet
+let morseKeys;
 
 // difficulty = digits
 function binaryArrayToStr(arr) { // function for printing arrays without commas
 	let s = "";
 	for (let i = 0; i < arr.length; i++) {
 		s = s.concat(String(arr[i]));
-		s = s.concat(" ");
+		//s = s.concat(" ");
 	}
 	return s;
 }
-function Generator(digits){ // generates a number and saves it in str form to binaryTxt as binary and b10Txt as base 10
-	let resD = 0;
+function Generator(){ // generates a number and saves it in str form to binaryTxt as binary and b10Txt as base 10
+	let resD = "";
+	correctAnswer = "";
+	let digits = Math.ceil(Math.random()*2);
 	for(let x in range(digits)){
-		correctAnswer[parseInt(x)] = floor(Math.random()*2);
-		resD += correctAnswer[parseInt(x)] * Math.pow(2, digits - parseInt(x) - 1);
+		let elem = morseKeys[Math.floor(Math.random()*morseKeys.length)];
+		correctAnswer = correctAnswer.concat(elem);
+		correctAnswer = correctAnswer.concat('/');
+		resD = resD.concat(morseToABC[elem]);
 	}
 	//txt = String(resD);
-	binaryTxt = binaryArrayToStr(correctAnswer);
-	b10Txt = String(resD);
+	binaryTxt = correctAnswer;
+	b10Txt = resD;
 }
 
 function update() {
@@ -60,8 +108,10 @@ function update() {
 		nextQ = true;
 		timeout = 0;
 		answer = [];
-		correctAnswer = [];
+		//correctAnswer = [];
 		binaryTxt = "testing";
+		morseKeys = Object.keys(morseToABC);
+		return;
 	}
 	// Gen prompt
 	if (nextQ) {
@@ -71,13 +121,15 @@ function update() {
 		}*/
 		timeout = questionTime;
 		// ask question
-		Generator(5);
+		Generator();
 		console.log(b10Txt);
 		console.log(binaryTxt);
 		console.log(correctAnswer);
 		nextQ = false;
+		paused = true;
 	}
 
+	// Visuals
 	color("red");
 	line(vec(0, 10), vec(timeout*100/questionTime, 10)); // timeout bar
 	text(String(floor(timeout*10/60)/10), vec(G.WIDTH/2-15, 4)); // timeout number
@@ -101,16 +153,19 @@ function update() {
 			bar(input.pos.x, input.pos.y, 14, 3, -Math.PI/4);
 		} else {
 			if (held > holdTime) {
-				color("yellow");
+				color("red");
 				//box(vec(input.pos), 10);
 				//arc(input.pos.x, input.pos.y, 6, 3);
-				bar(input.pos.x, input.pos.y, 10, 3, Math.PI/2);
+				bar(input.pos.x, input.pos.y, 10, 3, 0);
 			} else {
 				color("cyan");
 				//box(vec(input.pos), 10);
-				arc(input.pos.x, input.pos.y, 6, 3);
+				arc(input.pos.x, input.pos.y, 1, 3);
 			}
 		}
+	} else if (!paused) {
+		color("yellow");
+		bar(input.pos.x, input.pos.y, 10, 3, -Math.PI/4);
 	}
 
 	// Input Handling
@@ -119,33 +174,39 @@ function update() {
 		held = 0;
 	}
 	held += 1;
-	/*if (!isPressing && held > holdTime) {
+	if (!paused && !isPressing && !input.isJustReleased && held > holdTime*3) {
 		// third kind of input: pause between inputs (ie for breaks btwn morse letters)
 		// note that you'll have to make a variable that makes sure it only registers the pause once
-	}*/
+		answer.push("/");
+		console.log(answer);
+		paused = true;
+	}
 	if (input.isJustReleased) {
 		isPressing = false;
 		if(input.pos.y > G.HEIGHT*3/4) { // cancel input
 			console.log("canceled");
 			play("coin");
 			answer = [];
+			paused = true;
 		} else {
 			if (held > holdTime) { //held
 				console.log("held");
 				console.log(held);
 				play("laser");
-				answer.push(1);
+				answer.push("-");
 			} else { //tapped
 				play("select");
 				console.log("tapped");
 				console.log(held);
-				answer.push(0);
+				answer.push(".");
 			}
+			held = 0;
+			paused = false;
 		}
 		console.log(answer);
 	}
 
-	if (answer.length == 5) { // Check if answer correct
+	if (answer.length == correctAnswer.length) { // Check if answer correct
 		let correct = true;
 		// compare to correct answer
 		for (let i = 0; i < answer.length; i++) {
@@ -156,11 +217,12 @@ function update() {
 		if (correct) {
 			nextQ = true;
 			console.log("YIPPEEE!");
-			play("powerUp");
+			play("lucky");
 			score += timeout/100;
 		} else {
 			play("explosion");
 			console.log("YUH OH!");
+			paused = true;
 		}
 		// empty answer
 		answer = [];
